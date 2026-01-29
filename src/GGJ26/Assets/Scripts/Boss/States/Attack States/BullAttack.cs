d0bdc0a14255bc89a4a39261cafbc41b291d5436
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2026-01-29 22:58:30 +0200

    Update BullAttack.cs

--- src/GGJ26/Assets/Scripts/Boss/States/Attack States/BullAttack.cs
+++ src/GGJ26/Assets/Scripts/Boss/States/Attack States/BullAttack.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using DG.Tweening;
 
-public class BullAttack : MonoBehaviour
+public class BullAttack : StateBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject[] _chargeStartPositions;
+
+    Vector2 _chargeStartPos;
+    Vector2 _chargeEndPos;
+
+    Sequence _attackSequence;
+
+    public override void OnStateEnter()
     {
-        
+        base.OnStateEnter();
+        _chargeEndPos = _chargeStartPos = ChooseStartPosition();
+        _chargeEndPos.x *= -1;
+
+        AnimateAttack();
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector2 ChooseStartPosition()
     {
-        
+        //Go to the position opposite to the player's side of the screen
+        foreach (var positionGO in _chargeStartPositions)
+        {
+            Vector2 position = positionGO.transform.position;
+            if (Mathf.Sign(position.x) == -Mathf.Sign(PlayerController.Instance.transform.position.x))
+            {
+                return position;
+            }
+        }
+        return _chargeStartPositions[0].transform.position;
+    }
+
+    void AnimateAttack()
+    {
+        Debug.Log("Starting bull attack!");
+        Vector2 startPosition = Controller.Body.transform.position;
+        _attackSequence = DOTween.Sequence();
+        _attackSequence.Append(Controller.Body.DOMove(_chargeStartPos, 1.5f).SetEase(Ease.InOutSine))
+            .Append(Controller.Body.DOMove(_chargeEndPos, 1.5f).SetEase(Ease.InOutSine).SetDelay(0.75f))
+            .Append(Controller.Body.DOMove(startPosition, 1.5f).SetEase(Ease.InOutSine).SetDelay(0.5f))
+            .OnComplete(() => _isDone = true);
+        _attackSequence.Play();
     }
 }
