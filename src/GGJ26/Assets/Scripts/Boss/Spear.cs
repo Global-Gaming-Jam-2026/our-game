e7b9a721a54ef15dfa5286bcd8e73564ab4162e6
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2026-01-30 09:14:23 +0200

    Fixed Spear Attack Softlock

--- src/GGJ26/Assets/Scripts/Boss/Spear.cs
+++ src/GGJ26/Assets/Scripts/Boss/Spear.cs
@@ -7,28 +7,34 @@ public class Spear : MonoBehaviour
     Rigidbody2D _body;
     Vector3 _speed;
 
+    Sequence _launchSequence;
+
     private void OnEnable()
     {
+        _originalParent = transform.parent;
         float zAngle = transform.eulerAngles.z;
         transform.DORotate((zAngle + 360) * Vector3.forward, 0.5f, RotateMode.FastBeyond360);
     }
 
     public void LaunchDelayed(int index)
     {
-        _originalParent = transform.parent;
-        _body = GetComponent<Rigidbody2D>();
-        transform.DOLocalMove(transform.localPosition + 0.5f * transform.right, 0.3f).SetDelay(1.2f + 0.35f * index)
-            .OnComplete(() => LaunchSpear());
+        _launchSequence = DOTween.Sequence();
+        _launchSequence
+            .Append(transform.DOLocalMove(transform.localPosition + 0.5f * transform.right, 0.3f).SetDelay(1.2f + 0.35f * index)
+                .OnComplete(() => LaunchSpear()));
+        _launchSequence.Play();
     }
 
     void LaunchSpear()
     {
+        _body = GetComponent<Rigidbody2D>();
         transform.SetParent(null);
         DOTween.To(() => _speed, x => _speed = x, -transform.right * 12.5f, 0.5f).OnComplete(() => _body.linearVelocity = _speed);
     }
 
     private void OnBecameInvisible()
     {
+        _launchSequence.Kill();
         gameObject.SetActive(false);
         transform.parent = _originalParent;
     }
