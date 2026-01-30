bceedde90eebb0a269d54036d0f43c88036bbb7c
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2026-01-30 02:19:34 +0200

    Selecting attacks by weighted values

--- src/GGJ26/Assets/Scripts/Boss/States/Attack States/AttackState.cs
+++ src/GGJ26/Assets/Scripts/Boss/States/Attack States/AttackState.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class AttackState : StateBase
 {
     List<StateBase> _attackStates;
+    int[] _attackWeights;
+
+    private void Start()
+    {
+        SetupSubStates();
+    }
 
     public override void OnStateEnter()
     {
         base.OnStateEnter();
         _subState = null;
+        
+        ChooseRandomAttack();
+    }
+
+    private void SetupSubStates()
+    {
         _attackStates = GetComponentsInChildren<StateBase>().ToList();
         _attackStates.Remove(this);
-        ChooseRandomAttack();
+        _attackWeights = new int[_attackStates.Count];
+        for (int i = 0; i < _attackStates.Count; i++)
+        {
+            _attackWeights[i] = 1;
+        }
     }
 
     void ChooseRandomAttack()
     {
-        int randomIndex = Random.Range(0, _attackStates.Count);
+        int randomIndex = WeightRandomSelector.ChooseRandomFromWeightedValues(_attackWeights);
+        RebalanceWeights(randomIndex);
         _subState = _attackStates[randomIndex];
         _subState.OnStateEnter();
+    }
+
+    void RebalanceWeights(int indexToExclude)
+    {
+        for (int i = 0; i < _attackWeights.Length; i++)
+        {
+            if (i == indexToExclude)
+                continue;
+            _attackWeights[i]++;
+        }
     }
 
     public override void OnStateUpdate()
