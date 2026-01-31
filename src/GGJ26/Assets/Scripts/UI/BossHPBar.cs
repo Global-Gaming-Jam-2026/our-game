0a162fb97c611a1aeead47c10e9d6ac8459109c3
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2026-01-31 15:14:05 +0200

    HP Bars

--- src/GGJ26/Assets/Scripts/UI/BossHPBar.cs
+++ src/GGJ26/Assets/Scripts/UI/BossHPBar.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 
 public class BossHPBar : MonoBehaviour
 {
-    [SerializeField] Image _hpBarImage;
-    [SerializeField] RectTransform _hpBarRect;
+    [SerializeField] SpriteRenderer _hpBarImage;
+    [SerializeField] Sprite[] _hpSprites;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +15,33 @@ public class BossHPBar : MonoBehaviour
 
     void UpdateBossHP(float percentage)
     {
-        _hpBarRect.DOShakePosition(0.3f, 20f, 30);
-        _hpBarImage.fillAmount = percentage;
+        int index = 0;
+        
+        if (percentage <= 0)
+        {
+            index = 5;
+        }
+        else if (percentage <= 0.1f)
+        {
+            index = 4;
+        }
+        else if (percentage <= 0.2f)
+        {
+            index = 3;
+        }
+        else if (percentage <= 0.4f)
+        {
+            index = 2;
+        }
+        else if (percentage <= 0.6f)
+        {
+            index = 1;
+        }
+        else if (percentage <= 0.8f)
+        {
+            index = 0;
+        }
+
+        _hpBarImage.sprite = _hpSprites[index];
     }
 }
